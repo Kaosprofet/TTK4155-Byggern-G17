1f76ccd7a284f63a6411f08b90e6d2485bde0c0f
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-08-31 14:06:22 +0200

    Initialised Microchip Studio project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * TTK4155-Byggern-G17.c
+ *
+ * Created: 31/08/2021 14:03:31
+ * Author : henri
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
