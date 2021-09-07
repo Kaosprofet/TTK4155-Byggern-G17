7225b91c75005b99fdeec5ae2adaee1b35c41e83
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-09-07 09:27:48 +0200

    uart tweaks

--- uart.c
+++ uart.c
@@ -20,7 +20,7 @@ void initUart() {
 
     
     UCSR0B = (1 << TXEN0) | (1 << RXEN0);   // Recieve and transmit enabled
-    UCSR0C = (1 << UCSZ00) | (1 << UCSZ10); // 8 bit data recieve
+    UCSR0C = (1 << UCSZ00) | (1 << UCSZ10); // 8 bit data recieve (UCSZ10)
     UCSR0C = (1 << USBS0) | (1 << URSEL0);   // 2 stop bits
 }
 
