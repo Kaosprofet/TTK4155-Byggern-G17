47f0bc6911f34bd548dd42044dc2ee5f32a7183a
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-09-07 10:53:34 +0200

    structure

--- uart.c
+++ uart.c
@@ -8,13 +8,9 @@
 
 #define BAUD_PRESCALE ((F_CPU / (BAUD * 16UL)) - 1)
 
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-#endif
-
 #include <avr/io.h>
+#include "functions.h"
 #include "uart.h"
-
 
 void initUart() {
 
