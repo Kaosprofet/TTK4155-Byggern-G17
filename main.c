97f145721c078d0b0a388b258b4c6ad01e9b5d69
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-09-07 09:02:09 +0200

    uart tweak

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
  * TTK4155-Byggern-G17.c
  *
  * Created: 31/08/2021 14:03:31
- * Author : henri
+ *
  */ 
+
+#ifndef F_CPU // In case F_CPU not defined
+#define F_CPU 4915200 // 4.9152 MHz
+#endif
 
 #include <avr/io.h>
 #include "uart.h"
