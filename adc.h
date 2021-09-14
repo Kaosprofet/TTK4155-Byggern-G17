3fbe8c5a553e76eae5589efa414c2c874e7d65f3
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-09-14 08:44:26 +0200

    temp return 0 added in ADC

--- adc.h
+++ adc.h
@@ -1,10 +1,9 @@
 #ifndef ADC_H_
 #define ADC_H_
 
-typedef enum {channel1, channel2, channel3, channel4} channel_t;
 
 void initADC(void);
-void selectADCChannel(channel_t channel);
+void selectADCChannel(uint8_t channel);
 char ADCRead(void);
 
 #endif
