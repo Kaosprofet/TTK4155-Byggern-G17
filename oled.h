61c01ca89ee17957372ec66e163abc88e56cc0aa
victormelhuus <victor.melhuus@gmail.com>
2021-09-21 17:02:08 +0200

    litt mer OLED stuff, fortsatt masse mangler

--- oled.h
+++ oled.h
@@ -4,5 +4,14 @@
 
 void initOLED(void);
 void writeCMD(uint8_t cmd);
-void writeDATA(uint8_t data)
+void writeDATA(uint8_t data);
+void printString(char inString);
 
+void oled_goto_page(uint8_t page);
+void oled_goto_col(uint8_t col);
+void oled_pos(uint8_t page, uint8_t col);
+void oled_reset();
+void oled_home();
+void oled_print(char*);
+
+typedef struct {int page, int col;} oled_position;
