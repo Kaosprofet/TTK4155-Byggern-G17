745749a952d0e66372050d3bacf4aae42c8a31f9
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-09-28 10:27:15 +0200

    Added read/write oled to SRAM

--- oled.c
+++ oled.c
@@ -13,6 +13,8 @@
 
 #define height 64
 #define width 128
+#define num_pages 8
+#define SRAM_address_start 0
 
 
 volatile char * OLED_CMD_val = (char *) OLED_CMD;
@@ -44,6 +46,21 @@ void writeCMD(uint8_t cmd){
 //Writes the input data to the data register of the OLED
 void writeDATA(uint8_t data){
 	OLED_DATA_val[0] = data;
+}
+
+// Writes data to SRAM with address based on current page and column 
+void writeDataSRAM(uint8_t data) {
+	uint8_t address = SRAM_address_start+position.col+width*position.page;
+	writeSRAM(address, data);
+}
+
+// Prints entire screen from SRAM memory
+void printFromSRAM(void) {
+	for (uint16_t address = SRAM_address_start; address < SRAM_address_start+(width * num_pages); address++) {
+		uint8_t data = readSRAM(address);
+		oled_pos(address / width, address % width);
+		writeDATA(data);
+	}
 }
 
 //Initialization
