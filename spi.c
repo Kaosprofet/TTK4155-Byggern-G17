60e3996b949d3bc010b9f5d050c81f3aeca8d201
oscarbm1998 <oscar.mork@hotmail.com>
2021-09-28 16:06:45 +0200

    spi/can

--- spi.c
+++ spi.c
@@ -5,22 +5,19 @@
 void spi_init(void)
 {
     setBit(MCUCR, SRE); //Enable peripherial pinmode
+    setBit(DDRB, PB5);  //Enable MOSI as output
+    setBit(DDRB, PB7);  //Enable SCK as output
+    setBit(DDRB, PB4);  //Enable SS as output
     setBit(SPCR, SPE);  //Enable SPI
     setBit(SPCR, MSTR); //Enable master mode
     setBit(SPCR, SPR0); //Sets the SCK frequency to Fclk/16
     clearBit(SPCR, SPR1);
-    clearBit(DDRB, PB6);  //Enable MISO as input
-    setBit(DDRB, PB5);    //Enable MOSI as output
-    setBit(DDRB, PB7);    //Enable SCK as output
-    setBit(DDRB, PB4);    //Enable SS as output
     clearBit(SPSR, SPI2X); //Sets SPI transfer speed to normal
 }
 
-void spi_transfer(void)
+void spi_transfer(char cData)
 {
-	clearBit(PORTB,PB4);
-    SPDR = 0b01011010; //Writes data for the SPI to transfer
+    SPDR = cData; //Writes data for the SPI to transfer
 
     waitUntilBitIsSet(SPSR, SPIF); //Recieves confirmation of finished transfer
-	setBit(PORTB,PB4);
 }
