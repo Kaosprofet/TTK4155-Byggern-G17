4232f66d0983d00c29570f4e2ac4a9091f679f89
oscarbm1998 <oscar.mork@hotmail.com>
2021-10-05 16:51:02 +0200

    Final changes before bed

--- can_controller.c
+++ can_controller.c
@@ -4,13 +4,12 @@
 
 uint8_t can_controller_read(uint8_t address)
 {
-    clearBit(PORTB, PB4); //Lower chip-select
-
-    spi_write(0b00000011); //Read data from register beginning at selected address
-    spi_write(address);    //The address from where to begin reading data
-    uint8_t value = spi_read();    //Reads data
-    setBit(PORTB, PB4);    //Raise chip-select
-    return value;          //Return the SPDR register which contains data read
+    clearBit(PORTB, PB4);       //Lower chip-select
+    spi_write(0b00000011);      //Read data from register beginning at selected address
+    spi_write(address);         //The address from where to begin reading data
+    uint8_t value = spi_read(); //Reads data
+    setBit(PORTB, PB4);         //Raise chip-select
+    return value;               //Return the SPDR register which contains data read
 }
 
 void can_controller_write(uint8_t address, uint8_t data)
@@ -28,7 +27,7 @@ void can_controller_request_to_send_TXB0(uint8_t address)
     spi_write(address);
     setBit(PORTB, PB4); //Lower chip-select
 }
-uint8_t can_controller_read_status(uint8_t address)
+uint8_t can_controller_read_status(void)
 {
     clearBit(PORTB, PB4);  //Lower chip-select
     spi_write(0b10100000); //OP code for reading tx/rx register status
@@ -50,18 +49,25 @@ void can_controller_reset(void)
     setBit(PORTB, PB4);    //Raise chip-select
 }
 
+void can_set_mode(void)
+{
+    clearBit(PORTB, PB4); //Lower chip-select
+    can_controller_write(0b11110000, 0b00000000);
+    setBit(PORTB, PB4); //Raise chip-select
+}
+
 void can_controller_init(void)
 {
-    spi_init(); // Initiate spi
+    spi_init();             // Initiate spi
     can_controller_reset(); // Reset the cancontroller
     _delay_ms(10);
     // Disable global interrupts
-	cli();
-	// Interrupt on falling edge PD2
-	setBit(MCUCR, ISC01);
-	clearBit(MCUCR, ISC00);
-	// Enable interrupt on PD2
-	setBit(GICR,INT0);
-	// Enable global interrupts
-	sei();
+    cli();
+    // Interrupt on falling edge PD2
+    setBit(MCUCR, ISC01);
+    clearBit(MCUCR, ISC00);
+    // Enable interrupt on PD2
+    setBit(GICR, INT0);
+    // Enable global interrupts
+    sei();
 }
