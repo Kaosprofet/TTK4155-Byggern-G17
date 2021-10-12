1c6dd59f55185826c805166970286ddce0916520
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-10-12 14:00:59 +0200

    Led test working

--- Node2/node2/main.c
+++ Node2/node2/main.c
@@ -7,15 +7,38 @@
 
 
 #include "sam.h"
+#include "functions.h"
 
 
-int main(void)
-{
+#define LED_1 PIO_PA19
+#define LED_2 PIO_PA20
+
+void led_test(void);
+
+
+int main(void) {
     /* Initialize the SAM system */
     SystemInit();
-
-    /* Replace with your application code */
-    while (1) 
-    {
+	
+	led_test();
+	
+	
+	
+	
+    while (1) {
+		
     }
+}
+
+void led_test(void) {
+	PIOA->PIO_PER |= LED_1;
+	PIOA->PIO_OER |= LED_1;
+	PIOA->PIO_PUDR |= LED_1;
+	
+	PIOA->PIO_PER |= LED_2;
+	PIOA->PIO_OER |= LED_2;
+	PIOA->PIO_PUDR |= LED_2;
+	
+	setBit(PIOA, LED_1);
+	clearBit(PIOA, LED_2);
 }
