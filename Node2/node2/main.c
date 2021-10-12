bf0a97c12a6b38731238cc9c3695d9123157e534
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-10-12 10:31:25 +0200

    node2 created

---
+++ Node2/node2/main.c
@@ -0,0 +1,21 @@
+/*
+ * node2.c
+ *
+ * Created: 12/10/2021 10:28:43
+ * Author : henri
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
