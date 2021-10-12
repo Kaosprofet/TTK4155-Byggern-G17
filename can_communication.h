32b7d8968829ad1374b1fbcd7ae7ee44565fd097
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-10-12 17:42:48 +0200

    Made interupt flag external

--- can_communication.h
+++ can_communication.h
@@ -3,6 +3,8 @@
 
 #include <avr/io.h>
 
+extern volatile uint8_t can_interrupt_flag;
+
 typedef struct{unsigned int ID; uint8_t length; uint8_t data[8];} can_message;
 void CAN_test(void);	
 void CAN_com_init(uint8_t can_mode); // Initialization
