5915b281136476d63bf53ffc28f070721e0d716a
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-10-19 22:00:02 +0200

    PWM clock initialised

--- Node2/node2/servo.c
+++ Node2/node2/servo.c
@@ -2,12 +2,28 @@
 #include "includes.h"
 #endif
 
-
+#define servo_max 150
+#define servo_min -150
+#define servo_offset 0
 
 void init_servo(void) {
 	
+	// Disable interrupts
+	PIOC->PIO_IDR = PIO_PC19B_PWMH5;
+	
+	//Enable Clock for PWM in PMC
+	PMC->PMC_PCR = PMC_PCR_EN | (0 << PMC_PCR_DIV_Pos) | PMC_PCR_CMD | (ID_PWM << PMC_PCR_PID_Pos);
+	PMC->PMC_PCER1 |= 1 << (ID_PWM - 32);
 }
 
-void position_servo(uint8_t position_servo()) {
+void position_servo(uint8_t position) {
+	position = position + servo_offset;
+	
+	if (position < servo_min) {
+		position = servo_min;
+	}
+	else if (position > servo_max) {
+		position = servo_max;
+	}
 	
 }
