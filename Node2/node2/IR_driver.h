7728c1d09c075beeaa1d7573d1aef92f5bbc638c
Victor <victor.melhuus@gmail.com>
2021-10-19 17:31:31 +0200

    Påbegynt ADC

--- Node2/node2/IR_driver.h
+++ Node2/node2/IR_driver.h
@@ -2,10 +2,13 @@
 #define IR_driver_H_
 
 #define IR_DIODE 23 //*
+//Pin 44 at PC19
+
 #define IR_BLOCK_THRESHOLD 500 //*
 
+void IR_init(void);
 uint8_t IR_raw(void); //Reads the variable***
-uint8_t IR_read(uint8_t ir_value); //Filters the data**
+uint8_t digitalFilter(uint8_t ir_value); //Filters the data**
 int IR_blocked(uint8_t ir_value); //returns 1 if it gets blocked**
 
 
