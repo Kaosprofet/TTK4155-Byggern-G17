b98bb8e355db1ee96ff4fba06e2269de85854a87
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-10-26 08:14:07 +0200

    PWM/servi init ferdig, mangler noen av konstantene

--- Node2/node2/servo.c
+++ Node2/node2/servo.c
@@ -14,6 +14,18 @@ void init_servo(void) {
 	//Enable Clock for PWM in PMC
 	PMC->PMC_PCR = PMC_PCR_EN | (0 << PMC_PCR_DIV_Pos) | PMC_PCR_CMD | (ID_PWM << PMC_PCR_PID_Pos);
 	PMC->PMC_PCER1 |= 1 << (ID_PWM - 32);
+
+	//Chanel mode (Her setter vi divisorverdien)
+	PWM->PWM_CH_NUM[5].PWM_CMR = PWM_CMR_CPOL|PWM_CMR_CPRE_MCK_DIV_32;
+
+	//Period
+	PWM->PWM_CH_NUM[5].PWM_CPRD = 52500;
+
+	//Center pos
+	PWM->PWM_CH_NUM[5].PWM_CDTY = 4331;
+
+	//Enable PWM (Vet ikke hva vi har definert som enable)
+	PWM->PWM_ENA = PWM_ENA_CHID5;
 }
 
 void position_servo(uint8_t position) {
