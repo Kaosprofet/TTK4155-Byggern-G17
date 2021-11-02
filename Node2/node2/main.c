1772ed9fc0cf58fb68ea72b98beda60272c6c148
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-11-02 21:24:04 +0100

    Updated inits + better LED test

--- Node2/node2/main.c
+++ Node2/node2/main.c
@@ -12,24 +12,29 @@
 #define LED_1 PIO_PA19
 #define LED_2 PIO_PA20
 
-
-
 void led_test(void);
 void can_test(void);
+void inits(void);
 
 
 int main(void) {
-    /* Initialize the SAM system */
-    SystemInit();
+	inits();
+	
+	//led_test();
+	//can_test();
+	
+	start_game();
+}
+
+void inits(void) {
+	/* Initialize the SAM system */
+	SystemInit();
 	WDT->WDT_MR = WDT_MR_WDDIS;	// Watchdog disabled
 	configure_uart();
 	init_can();
 	init_game_board();
 	DAC_init();
-	controller.button_state=0;
-	led_test();
-	//can_test();
-	start_game();
+	controller =  (Controller){0, 0, 0, 0, 0};
 }
 
 void led_test(void) {
@@ -41,7 +46,12 @@ void led_test(void) {
 	PIOA->PIO_OER |= LED_2;
 	PIOA->PIO_PUDR |= LED_2;
 	
-	//setBit(PIOA, LED_1);
+	setBit(PIOA, LED_1);
+	clearBit(PIOA,LED_2);
+	while(RTT->RTT_VR<RTT->RTT_VR+100){}
+	setBit(PIOA, LED_2);
+	clearBit(PIOA,LED_1);
+	while(RTT->RTT_VR<RTT->RTT_VR+100){}
 
 }
 void can_test(void) {
