d84714aa14137cdf8921d08d31274154b89d96d4
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-11-02 15:44:57 +0100

    dac work

--- Node2/node2/DAC.c
+++ Node2/node2/DAC.c
@@ -7,10 +7,15 @@ void DAC_init(void){
     DACC->DACC_MR=DACC_MR_TRGEN_DIS;//Set DAC mode freerun (no clock prescaler)
 	DACC->DACC_MR=DACC_MR_WORD_HALF;//Sets the DAC to read only [15-0] bits in the DACC_CDR register
     DACC->DACC_CHER = DACC_CHER_CH1; //DAC sends output on DAC 1
+	DACC->DACC_MR = DACC_MR_USER_SEL_CHANNEL1;
 	DACC->DACC_MR = DACC_MR_STARTUP_0; //Sets the DAC startup wait timer to 0
 }
 
 void DAC_set_output(void){
-	uint16_t re_mapped_y= map(controller.y, -100, 100, 0, 65535);
-    DACC->DACC_CDR = re_mapped_y; //Sets the value in the DACC_CDR register to be the re_mapped_value. 
+	//printf("%d\n\r",controller.y);
+	uint16_t re_mapped_y= map(controller.y, -100, 100, 0, 4095);
+	if (!DACC_IER_TXRDY){
+    DACC->DACC_CDR = re_mapped_y; //Sets the value in the DACC_CDR register to be the re_mapped_value.
+	}
+	printf("%d\n\r",DACC_ISR_EOC);
 }
