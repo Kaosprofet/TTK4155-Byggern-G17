ef09a02222fab0eb96b8fb838a1371f7c4557c70
Victor <victor.melhuus@gmail.com>
2021-11-09 17:41:27 +0100

    Fungerende motordriver

--- Node2/node2/motor_driver.h
+++ Node2/node2/motor_driver.h
@@ -1,12 +1,9 @@
 #ifndef MOTOR_DRIVER_H_
 #define MOTOR_DRIVER_H_
 
-//PI controller
-#define Kp 0.01
-#define Ti 1
-#define N 10
 
 
+int16_t PI_controller(int16_t r, int16_t y);
 
 void motor_controll_init(void);
 void motor_controll(void);
