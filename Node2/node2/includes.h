b1c9c0ae7ee3d6a7419d3b5bcb82d8d01f493ea4
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-11-18 10:50:21 +0100

    Removed reference to not used files

--- Node2/node2/includes.h
+++ Node2/node2/includes.h
@@ -36,8 +36,6 @@ Game game;
 #include "can_interrupt.h"
 #include "can_message.h"
 #include "printf.h"
-#include "servo.h"
-#include "solenoid.h"
 #include "game.h"
 #include "game_board_driver.h"
 #include "DAC.h"
