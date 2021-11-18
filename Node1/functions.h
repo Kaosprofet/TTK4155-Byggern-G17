1c089c5fb4df067b7fd0238ef204b5a09c9b5355
Henrik M. Arnesen <henrimar@stud.ntnu.no>
2021-11-18 10:41:37 +0100

    Include guard added

--- Node1/functions.h
+++ Node1/functions.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
 #define setBit(reg, bit) (reg |= (1<<bit))
 #define clearBit(reg, bit) (reg &= ~(1<<bit))
 #define toggleBit(reg, bit) (reg ^= (1<<bit)) 
@@ -6,3 +9,5 @@
 
 #define waitUntilBitIsSet(reg, bit) while(!bitIsSet(reg,bit))
 #define waitUntilBitIsClear(reg, bit) while(bitIsSet(reg,bit))
+
+#endif
